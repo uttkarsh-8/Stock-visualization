301200b2660b2b1d22726d810adac16d71d1ba02
uttkarsh-8 <uttkarsh.srivastava12@gmail.com>
2024-11-12 15:30:49 +0530

    Add URL routes for stock visualizer and load environment variables

--- alphaVantage/urls.py
+++ alphaVantage/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import stockVisualizer.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", stockVisualizer.views.home),
+    path('get_stock_data/', stockVisualizer.views.get_stock_data),
 ]
--- stockVisualizer/views.py
+++ stockVisualizer/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import StockData
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 APIKEY = os.getenv('ALPHA_VANTAGE_API_KEY')
